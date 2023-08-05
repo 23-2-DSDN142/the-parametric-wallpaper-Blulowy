7a0faef6ba8e0c9bd5c5ea64fa4e16b87833d1cb
Blulowy <140657391+Blulowy@users.noreply.github.com>
2023-08-06 01:50:15 +1200

    Update my_wallpaper.js

--- my_wallpaper.js
+++ my_wallpaper.js
@@ -4,12 +4,13 @@ let mcenter = 200;
 let pwidh = 1;
 let phigh = 2.5;
 let size = 12;
+let strS = 1.5;
 //my colour parametrs are at the start of function 'my symbol'
 
 
 function setup_wallpaper(pWallpaper) {
   pWallpaper.output_mode(GRID_WALLPAPER);
-  pWallpaper.resolution(FIT_TO_SCREEN);
+  pWallpaper.resolution(NINE_PORTRAIT);
   pWallpaper.show_guide(false); //set this to false when you're ready to print
 
   //Grid settings
@@ -21,18 +22,36 @@ function setup_wallpaper(pWallpaper) {
 function wallpaper_background() {
   colorMode(HSB);
   angleMode(DEGREES)
-  background('#8FC0ED'); //light honeydew green colour
+  background('#8FC0ED'); 
 }
+
+
 
 function my_symbol() { // do not rename this function. Treat this similarly to a Draw function
   //my parameters for colours
-  let sunColor = color('#E37F1B');
-  let moonColor = color('#1A1697');
+  //let sunColor = color('#E37F1B');
+  let sunColor = color('#fa7e1e');
+  //let moonColor = color('#1A1697')
+  let bgNight = color('#455FA2');
+  let moonColor = color('#212C91');
   let bgColor = color('#8FC0ED');
+  let strCol = color('#7FA4DA');
+  
+  
+  //noStroke();
+  //fill(bgNight);
+  //noFill()
+ // triangle(110, 0, 0, 70, 0, -70);
+  //triangle(-110, 0, 0, 70, 0, -70);
+  
 
-  /* Sun-eye symbol */
+  /* 
+  * Sun-eye symbol 
+  */
   fill(sunColor);
-  noStroke();
+  //noStroke();
+  stroke(strCol);
+  strokeWeight(strS);
 
   push();
   translate(scenter,scenter)
@@ -65,30 +84,48 @@ function my_symbol() { // do not rename this function. Treat this similarly to a
 
 
   fill(sunColor);
-  noStroke();
+  //noStroke();
+  stroke(strCol);
+  strokeWeight(strS);
   ellipse(scenter, scenter, 7.5*size, 7*size); 
 
 
   fill(bgColor);
-  noStroke();
+  //noStroke();
   ellipse(scenter, scenter, pwidh*size, phigh*size);
 
 
      
-  /* Moon-eye symbol */
+  /* 
+  * Moon-eye symbol 
+  */
   fill(moonColor);
-  noStroke();
+  //noStroke();
+  stroke(strCol);
+  strokeWeight(strS);
   ellipse(mcenter+mcenter, mcenter+mcenter, 7.5*size, 7*size); 
 
   fill(bgColor);
   noStroke();
   ellipse(mcenter-20, mcenter, 7.5*size, 6.5*size); // center-20 
+  
+  stroke(strCol);
+  strokeWeight(strS);
+  noFill();
+  arc(mcenter-20, mcenter, 7.5*size, 6.5*size, 270, 90)
 
   fill(moonColor);
-  noStroke();
+  //noStroke();
+  stroke(strCol);
   ellipse(mcenter, mcenter, pwidh*size, phigh*size);
 
-  /* lines */
+  
+
+
+
+  /* 
+  * lines 
+  */
   strokeWeight(5);
   stroke('#7FA4DA');
   line(100, -10, 0, -70);
@@ -102,6 +139,27 @@ function my_symbol() { // do not rename this function. Treat this similarly to a
   line(-35, -60, 0, -80);
   line(0, 80, 35, 60);
   line(-35, 60, 0, 80);
+
+  stroke('#7FA4DA');
+  line(92, 0, 77, 10);
+  line(92, 0, 77, -10);
+  line(109, 0, 124, 10);
+  line(109, 0, 124, -10);
+  stroke('#7390CD')
+  fill('#7390CD');
+  circle(100, 0, 5, 5);
+
+  /* eye */
+  stroke('#7FA4DA');
+  noFill();
+  /*arc(0, 103, 20, 15, 0, 180);
+  arc(0, 101, 20, 20, 180, 0);
+  circle(0, 101, 1);*/
+
+  ellipse(0, 100, 22, 20)
+  line(0, 101, 0, 99)
+
+   
 
 }
 
