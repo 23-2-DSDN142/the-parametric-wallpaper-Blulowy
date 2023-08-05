253c70cb13f1652113ebd6355ec2d9d6eeb56014
Blulowy <140657391+Blulowy@users.noreply.github.com>
2023-08-05 15:48:54 +1200

    Update my_wallpaper.js

--- my_wallpaper.js
+++ my_wallpaper.js
@@ -1,10 +1,9 @@
 //your parameter variables go here!
-let rect_width  = 20;
-let rect_height = 20;
-let center = 70;
+let scenter = 100;
+let mcenter = 200;
 let pwidh = 1;
 let phigh = 2;
-let size = 9;
+let size = 12;
 //my colour parametrs are at the start of function 'my symbol'
 
 
@@ -16,7 +15,7 @@ function setup_wallpaper(pWallpaper) {
   //Grid settings
   pWallpaper.grid_settings.cell_width  = 200;
   pWallpaper.grid_settings.cell_height = 200;
-  pWallpaper.grid_settings.row_offset  = 50;
+  pWallpaper.grid_settings.row_offset  = 0;
 }
 
 function wallpaper_background() {
@@ -36,14 +35,14 @@ function my_symbol() { // do not rename this function. Treat this similarly to a
   noStroke();
 
   push();
-  translate(center,center)
+  translate(scenter,scenter)
   rotate(45);
   triangle(-1.5*size, -3*size, 1*size, -5.5*size, 2*size, -3.5*size); //up
   triangle(-2*size, 3*size, -1*size, 5.5*size, 1.5*size, 3*size); //down
   pop();
 
   push();
-  translate(center,center)
+  translate(scenter,scenter)
   rotate(-65);
   triangle(-1.5*size, -3*size, 1*size, -5.5*size, 2*size, -3.5*size); //up
   triangle(-2*size, 3*size, -1*size, 5.5*size, 1.5*size, 3*size); //down
@@ -55,44 +54,39 @@ function my_symbol() { // do not rename this function. Treat this similarly to a
   //triangle(8, 13.5, center, 15.5, 12, 13.5); //down
 
   push();
-  translate(center,center)
+  translate(scenter,scenter)
   triangle(-2*size, -3.5*size, 0*size, -5.5*size, 2*size, -3.5*size); //up
   triangle(-2*size, 3.5*size, 0*size, 5.5*size, 2*size, 3.5*size); //down
   pop()
 
   fill(bgColor);
   noStroke();
-  circle(center, center, 8*size);
+  circle(scenter, scenter, 8*size);
 
 
   fill(sunColor);
   noStroke();
-  ellipse(center, center, 7.5*size, 7*size); 
+  ellipse(scenter, scenter, 7.5*size, 7*size); 
+
+
   fill(bgColor);
   noStroke();
-  if(mouseX==center && mouseY==center){
-     ellipse(center, center, pwidh*size*1.5, phigh*size/2);
-  }
-  else{
-     ellipse(center, center, pwidh*size, phigh*size);}
+  ellipse(scenter, scenter, pwidh*size, phigh*size);
 
+  
+     
   /* Moon-eye symbol */
   fill(moonColor);
   noStroke();
-  ellipse(center+center, center+center, 7.5*size, 7*size); 
+  ellipse(mcenter+mcenter, mcenter+mcenter, 7.5*size, 7*size); 
 
   fill('#8FC0ED');
   noStroke();
-  ellipse(center+(center-0.2*center), center+center, 7.5*size, 6.5*size); // center-20 
+  ellipse(mcenter-20, mcenter, 7.5*size, 6.5*size); // center-20 
 
   fill(moonColor);
   noStroke();
-if(mouseX==center*2 && mouseY==center*2){
-  ellipse(center+center, center+center, pwidh*size*1.5, phigh*size/2);
-}
-else{
-  ellipse(center+center, center+center, pwidh*size, phigh*size);
-}
+  ellipse(mcenter, mcenter, pwidh*size, phigh*size);
 
 }
 
